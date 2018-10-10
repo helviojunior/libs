1b54970b03073be6caf388947e5e587fefed6bc3
Helvio Junior (M4v3r1cK) <34519097+helviojunior@users.noreply.github.com>
2018-10-10 10:31:14 -0300

    Update install_kali.sh

--- Bash/install_kali.sh
+++ Bash/install_kali.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ "$(id -u)" != "0" ]; then
+    echo "Please, run this script as root"
+    exit 1
+fi
+
 ##########################################
 ## Atualiza pacotes instalados
-sudo apt-get update && apt-get upgrade
+apt-get update && apt-get upgrade
 
 ##########################################
 ## Recomendado atualizar todo o sistema
@@ -12,44 +17,59 @@ sudo apt-get update && apt-get upgrade
 ## Instala pacotes utilizados
 
 # Pacotes head do kernel atual
-sudo apt-get install -y linux-headers-$(uname -r)
+apt-get install -y linux-headers-$(uname -r)
 
 # Pacotes de desenvolvimento
-sudo apt-get install -y libc6 gcc g++ pip pip3 clinfo pkg-config g++-multilib libc6-dev-i386 autoconf openssl libssl-dev pwntools python python3
+apt-get install -y libc6 gcc g++ pip pip3 clinfo pkg-config g++-multilib libc6-dev-i386 autoconf openssl libssl-dev pwntools python python3
 
 # Outras ferramentas
-sudo apt-get install -y hashcat-utils hashcat deepin-screenshot keepassx netcat-traditional
+apt-get install -y hashcat-utils hashcat deepin-screenshot keepassx netcat-traditional
 
 # Atualiza o sistema para usar no NC tradicional ao invés do que vem no Kali
-sudo rm /etc/alternatives/nc && sudo ln -s /bin/nc.traditional /etc/alternatives/nc
+rm /etc/alternatives/nc && ln -s /bin/nc.traditional /etc/alternatives/nc
 
 
 ##########################################
 ## Adiciona o Alias para tail
 echo "Criando alias para tailf"
-if [ "$(id -u)" != "0" ]; then
-    if [ "$(grep 'alias tailf' ~/.bashrc | wc -l)" == "0" ]; then
-        echo 'alias tailf="tail -f"' >> ~/.bashrc
-    fi
-fi
-if [ "$(sudo grep 'alias tailf' /root/.bashrc | wc -l)" == "0" ]; then
-    sudo echo 'alias tailf="tail -f"' >> /root/.bashrc
+if [ "$(grep 'alias tailf' /root/.bash_aliases 2>/dev/null | wc -l)" -eq "0" ]; then
+    echo 'alias tailf="tail -f"' >> /root/.bash_aliases
 fi
 
 ##########################################
 ## Habilita serviço de SSH
-sudo systemctl enable ssh.service
+systemctl enable ssh.service
 
 ##########################################
 ## Instala o peda no GDB
-if [ "$(id -u)" != "0" ]; then
-    if [ "$(grep 'peda.py' ~/.gdbinit | wc -l)" == "0" ]; then
-        git clone https://github.com/longld/peda.git ~/peda
-        echo "source ~/peda/peda.py" >> ~/.gdbinit
+if [ "$(grep 'peda.py' /root/.gdbinit 2>/dev/null | wc -l)" -eq "0" ]; then
+    git clone https://github.com/longld/peda.git /root/peda
+    echo "source ~/peda/peda.py" >> /root/.gdbinit
+fi
+
+
+############
+## Processos de usuários
+
+for u in $(getent passwd | cut -d: -f1); do
+    if [ -d "/home/$u" ]; then
+        echo "Path found to user $u"
+        
+        ##########################################
+        ## Adiciona o Alias para tail
+        echo "Criando alias para tailf"
+        if [ "$(grep 'alias tailf' /home/$u/.bash_aliases 2>/dev/null | wc -l)" -eq "0" ]; then
+            echo 'alias tailf="tail -f"' >> /home/$u/.bash_aliases
+        fi
+        
+        ##########################################
+        ## Instala o peda no GDB
+        if [ "$(grep 'peda.py' /home/$u/.gdbinit 2>/dev/null | wc -l)" -eq "0" ]; then
+            git clone https://github.com/longld/peda.git /home/$u/peda
+            echo "source ~/peda/peda.py" >> /home/$u/.gdbinit
+        fi
+        
     fi
-fi
-if [ "$(sudo grep 'peda.py' /root/.gdbinit | wc -l)" == "0" ]; then
-    sudo git clone https://github.com/longld/peda.git /root/peda
-    sudo echo "source /root/peda/peda.py" >> /root/.gdbinit
-fi
+done
+
 
