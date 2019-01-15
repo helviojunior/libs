833c3682ba505f02b0feee83813364d78a349177
Helvio Junior <helvio@localhost.localdomain>
2019-01-15 16:10:13 -0200

    XOR Tool

---
+++ python/xor.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+ 
+'''
+Author : Helvio Junior (M4v3r1cK)
+Date : 2019-01-15
+https://github.com/helviojunior/libs/blob/master/python/xor.py
+'''
+
+import os, re, sys, getopt, argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument('text', help='Text to encode with xor')
+parser.add_argument('key', type=int, help='xor key')
+
+args = parser.parse_args()
+
+key = int(args.key)
+if key < 0:
+    key = 0
+
+if key > 255:
+    key = 255
+
+text=args.text
+
+text2 = ""
+
+for i in range(len(text)):
+    text2 += chr(ord(text[i]) ^ key)
+
+print(text2, end='', flush=True)
+
