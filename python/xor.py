bf515c86fc383f20f137fb3e541dadd32619c0c5
Helvio Junior (M4v3r1cK) <34519097+helviojunior@users.noreply.github.com>
2021-09-17 08:55:25 -0300

    Implementation formats

--- python/xor.py
+++ python/xor.py
@@ -4,6 +4,7 @@
 '''
 Author : Helvio Junior (M4v3r1cK)
 Date : 2019-01-15
+Updated: 2021-09-17
 https://github.com/helviojunior/libs/blob/master/python/xor.py
 '''
 
@@ -11,11 +12,12 @@ import os, re, sys, getopt, argparse
 import sys, struct
 
 parser = argparse.ArgumentParser()
-parser.add_argument('text', help='Text to encode with xor')
-parser.add_argument('key', help='xor key')
+parser.add_argument('-text', help='Text to encode with xor')
+parser.add_argument('-key', help='xor key')
+parser.add_argument('-f', '--format', default='raw', help='Format to output (c, csharp, python)')
+parser.add_argument('-v', '--variable', default='buffer', help='Buffer variable Name')
 
 args = parser.parse_args()
-
 
 def print_err(text):
     sys.stderr.write(text)
@@ -24,6 +26,54 @@ def print_err(text):
 def print_std(data):
     sys.stdout.buffer.write(data)
     sys.stdout.flush()
+
+def print_output(data):
+
+    fmt = args.format
+    if fmt != "raw":
+        var_name = args.variable
+
+        if fmt == "c":
+            txtdata = "unsigned char %s[] =" % var_name
+            txtdata += "\n\""
+            for idx, val in enumerate(data):
+
+                if idx != 0 and idx % 16 == 0:
+                    txtdata += "\"\n\""
+
+                txtdata += "\\x{0:02x}".format(val)
+
+            txtdata += "\"\n"
+            print(txtdata)
+
+        elif fmt == "csharp":
+            txtdata = "byte[] %s = new byte[%d] {" % (var_name, len(data))
+            for idx, val in enumerate(data):
+
+                if idx % 16 == 0:
+                    txtdata += "\n"
+
+                txtdata += "0x{0:02x},".format(val)
+
+            txtdata = txtdata.strip(",")
+            txtdata += " };\n"
+            print(txtdata)
+
+        elif fmt == "python":
+            txtdata = "%s =  b\"\"\n" % var_name
+            for idx, val in enumerate(data):
+
+                if idx % 16 == 0:
+                    txtdata += "%s += b\"" % var_name
+
+                txtdata += "\\x{0:02x}".format(val)
+
+            txtdata = txtdata.strip(",")
+            txtdata += "\"\n"
+            print(txtdata)
+
+    else: # raw
+        print_std(data)
 
 
 ikey = int(args.key, 0)
@@ -50,4 +100,4 @@ odata = bytearray()
 for i in bdata:
     odata.append( i ^ key )
 
-print_std(odata)
+print_output(odata)
